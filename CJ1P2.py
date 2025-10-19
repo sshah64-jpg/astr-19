d761ed2672fc0ce0937e59d45af9770b72a9eab9
sshah64-jpg <sshah64@ucsc.edu>
2025-10-19 16:01:42 -0700

    Add files via upload
    Code Journal #1 Session Prompt 2

---
+++ CJ1P2.py
@@ -0,0 +1,20 @@
+num1 = 5.0
+num2 = 7.0
+
+sum = num1 + num2
+print(sum)
+print("Float")
+
+num3 = 5
+num4 = 7
+
+diff = num4 - num3
+print(diff)
+print("Integer")
+
+num5 = 5.0
+num6 = 7
+
+prod = num5 * num6
+print(prod)
+print("Float")
